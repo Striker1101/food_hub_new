1080005e51b327f6c54932780e61486651fc0c6d
striker1101 <goodluckodemilin@gmail.com>
2025-05-11 12:17:18 +0100

    change env

--- frontend/util/general_data.ts
+++ frontend/util/general_data.ts
@@ -1,7 +1,7 @@
 export const general_data = {
   name: "Food Hub",
-  backend_url: "http://10.0.2.2:5000",
-  // backend_url: "https://foodhub.erose.name.ng",
+  // backend_url: "http://10.0.2.2:5000",
+  backend_url: "https://foodhub.erose.name.ng",
   currency: "NGN",
   symbol: "₦",
 };
